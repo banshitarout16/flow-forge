1d35404622259387b3e90f8508319547c7d5a9ff
banshita rout <banshita.rout.1@gmail.com>
2026-08-22 20:55:42 +0530

    socket connectivity bug fix

--- backend/controllers/team.controller.js
+++ backend/controllers/team.controller.js
@@ -1,4 +1,5 @@
 import Team from "../models/Team.js";
+import User from "../models/User.js";
 import { asyncHandler } from "../middlewares/errorHandler.middleware.js";
 
 export const createTeam = asyncHandler(async (req, res) => {
@@ -47,5 +48,58 @@ export const deleteTeam = asyncHandler(async (req, res) => {
     res.status(404);
     throw new Error("Team not found");
   }
+  await User.updateMany({ organizationId: req.organizationId, teamId: team._id }, { teamId: null });
   res.json({ message: "Team deleted" });
+});
+
+export const addTeamMember = asyncHandler(async (req, res) => {
+  const { userId } = req.body;
+
+  const team = await Team.findOne({ _id: req.params.id, organizationId: req.organizationId });
+  if (!team) {
+    res.status(404);
+    throw new Error("Team not found");
+  }
+
+  const user = await User.findOne({ _id: userId, organizationId: req.organizationId });
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found in this organization");
+  }
+
+  if (team.members.some((m) => m.toString() === userId)) {
+    res.status(400);
+    throw new Error("This user is already a member of the team");
+  }
+
+  team.members.push(user._id);
+  await team.save();
+
+  user.teamId = team._id;
+  await user.save();
+
+  const populated = await team.populate("members", "name email role");
+  res.json(populated);
+});
+
+export const removeTeamMember = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
+
+  const team = await Team.findOne({ _id: req.params.id, organizationId: req.organizationId });
+  if (!team) {
+    res.status(404);
+    throw new Error("Team not found");
+  }
+
+  team.members = team.members.filter((m) => m.toString() !== userId);
+  await team.save();
+
+  const user = await User.findOne({ _id: userId, organizationId: req.organizationId });
+  if (user && user.teamId?.toString() === team._id.toString()) {
+    user.teamId = null;
+    await user.save();
+  }
+
+  const populated = await team.populate("members", "name email role");
+  res.json(populated);
 });
--- backend/routes/team.routes.js
+++ backend/routes/team.routes.js
@@ -1,5 +1,7 @@
 import express from "express";
-import { createTeam, getTeams, getTeamById, updateTeam, deleteTeam } from "../controllers/team.controller.js";
+import {
+  createTeam, getTeams, getTeamById, updateTeam, deleteTeam, addTeamMember, removeTeamMember,
+} from "../controllers/team.controller.js";
 import { protect } from "../middlewares/auth.middleware.js";
 import { tenantScope } from "../middlewares/tenantScope.middleware.js";
 import { roleCheck } from "../middlewares/roleCheck.middleware.js";
@@ -16,5 +18,8 @@ router.route("/:id")
   .get(getTeamById)
   .patch(roleCheck("org_admin", "manager"), updateTeam)
   .delete(roleCheck("org_admin"), deleteTeam);
+
+router.post("/:id/members", roleCheck("org_admin", "manager"), addTeamMember);
+router.delete("/:id/members/:userId", roleCheck("org_admin", "manager"), removeTeamMember);
 
 export default router;
